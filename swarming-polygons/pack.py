657b2b764604b36818e178ffb3ca5fe52f305e89
Tiago <tiagoloureirochaves@gmail.com>
2019-04-29 18:50:51 +0000

    Change array types and add constants

--- swarming-polygons/pack.py
+++ swarming-polygons/pack.py
@@ -4,17 +4,20 @@ from time import time
 from PIL import Image, ImageDraw
 
 WHITE = (255, 255, 255, 0)
+PRINT_CYCLE = 5000
+SAVE_CYCLE = 10000
+SAVE_PATH = "pack.png"
+DNA_PATH = "dna.txt"
 
 # ______________________________________________________________________________
 class Pack:
     def __init__(self, image, polygon_count, vertices_count, fitness_func):
-        self.width = image.shape[1]
-        self.height = image.shape[0]
+        self.height, self.width, *_ = image.shape # image.shape == (height, width, depth)
         self.vertices_count = vertices_count
 
         self.colors = np.random.randint(low=0, high=256, size=(polygon_count, 4), dtype=np.dtype('uint8')) # RGBA
 
-        self.polygons = np.empty((polygon_count, 2 * vertices_count), dtype=np.dtype('uint16'))
+        self.polygons = np.empty((polygon_count, 2 * vertices_count), dtype=np.dtype('int16'))
         self.polygons[:, 0::2] = np.random.randint(low=0, high=self.width, size=(polygon_count, vertices_count)) # x values on even positions
         self.polygons[:, 1::2] = np.random.randint(low=0, high=self.height, size=(polygon_count, vertices_count)) # y values on odd positions
         
@@ -36,14 +39,14 @@ class Pack:
         def __mutate_vertex(self, polygon_index):
             polygons = self.polygons.copy()
             vertex_index = 2 * np.random.randint(low=0, high=self.vertices_count) # chooses one of the polygon's vertices to mutate
-            polygons[polygon_index, vertex_index] = np.random.randint(low=0, high=self.width + 1, dtype=np.dtype('uint16'))
-            polygons[polygon_index, vertex_index + 1] = np.random.randint(low=0, high=self.height + 1, dtype=np.dtype('uint16'))
+            polygons[polygon_index, vertex_index] = np.random.randint(low=0, high=self.width + 1, dtype=np.dtype('int16'))
+            polygons[polygon_index, vertex_index + 1] = np.random.randint(low=0, high=self.height + 1, dtype=np.dtype('int16'))
             return self.colors, polygons
 
         def __mutate_polygon(self, polygon_index):
             polygons = self.polygons.copy()
-            polygons[polygon_index, 0::2] = np.tile(np.random.randint(low=0, high=self.width + 1, dtype=np.dtype('uint16')), self.vertices_count) # x's
-            polygons[polygon_index, 1::2] = np.tile(np.random.randint(low=0, high=self.height + 1, dtype=np.dtype('uint16')), self.vertices_count) # y's
+            polygons[polygon_index, 0::2] = np.tile(np.random.randint(low=0, high=self.width + 1, dtype=np.dtype('int16')), self.vertices_count) # x's
+            polygons[polygon_index, 1::2] = np.tile(np.random.randint(low=0, high=self.height + 1, dtype=np.dtype('int16')), self.vertices_count) # y's
             return self.colors, polygons
 
         def __mutate_color(self, polygon_index):
@@ -92,13 +95,17 @@ try:
     polygon_count = int(argv[2])
     vertices_count = 3 if len(argv) <= 3 else int(argv[3])
     max_cycles = -1 if len(argv) <= 4 else int(argv[4])
+    assert(polygon_count >= 0 and polygon_count <= 255)
+    assert(vertices_count >= 3 and vertices_count <= 255)
 except:
     print("usage: python swarming_polygons.py image_path polygon_count [vertices_count] [max_cycles]")
     exit()
 
 image = np.array(Image.open(image_path).convert('RGB')) # (3x8-bit pixels, true color)
+assert(image.shape[0] <= 4096 and image.shape[1] <= 4096)
+
 def fitness_ssd(pack_image):
-    return np.square(np.subtract(image, pack_image, dtype=np.dtype('uint16'))).sum() # TODO remove dtype
+    return np.square(np.subtract(image, pack_image, dtype=np.dtype('int16')).astype('int32')).sum() # sum square difference
 
 pack = Pack(image, polygon_count, vertices_count, fitness_ssd)
 
@@ -106,24 +113,20 @@ cycle = 0
 start_time = time()
 try:
     while cycle < max_cycles or max_cycles < 0:
-        if cycle % 5000 == 0:
-            if cycle % 10000 == 0 and cycle != 0: pack.save_image(f"{cycle}.png", 'PNG')
+        if cycle % PRINT_CYCLE == 0:
+            if cycle % SAVE_CYCLE == 0 and cycle != 0: pack.save_image(f"{cycle}.png", 'PNG')
             print(f"[{cycle}] fitness={pack.best_fitness}, Δt={(time() - start_time):.2f}s")
-        pack.cycle(fitness_ssd) # iterates through a cycle, returns (colors, polygons, image_array, fitness)
+        pack.cycle(fitness_ssd) # iterates through a cycle
         cycle += 1
 except:
     pass
 finally:
     end_time = time()
-    save_image_path = "pack.png"
-    pack.save_image(save_image_path, 'PNG')
+    pack.save_image(SAVE_PATH, 'PNG')
     print(f"[{cycle}] fitness={pack.best_fitness}, Δt={(time() - start_time):.2f}s")
-    print(f"\nSolution saved at {save_image_path}")
+    print(f"\nSolution saved at {SAVE_PATH}")
     print(f"[polygons|vertices|fitness|cycle|time]=[{polygon_count}|{vertices_count}|{pack.best_fitness}|{cycle}|{(end_time - start_time):.2f}]")
-
-    dna = pack.dna()
-    save_dna_path = "dna.txt"
-    f = open(save_dna_path,"w+")
-    f.write(dna)
+    f = open(DNA_PATH,"w+")
+    f.write(pack.dna())
     f.close()
-    print(f"\nSolution's DNA saved at {save_dna_path}")
+    print(f"\nSolution's DNA saved at {DNA_PATH}")
