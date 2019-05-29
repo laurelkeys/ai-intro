a6c7755f37745cfbee0a71ab0d0d9fb574feee3c
Tiago <tiagoloureirochaves@gmail.com>
2019-05-29 11:44:12 -0300

    Refactor duplicated code

--- boids/sketch.js
+++ boids/sketch.js
@@ -6,37 +6,36 @@
 // https://youtu.be/mhjuuHl6qHM
 // https://editor.p5js.org/codingtrain/sketches/ry4XZ8OkN
 
-const flock = [];
+const boids = [];
 
 let alignSlider, cohesionSlider, separationSlider;
 
 function setup() {
-  createCanvas(640, 480);
-  
-
-  sDiv = createDiv('Separation');
-  separationSlider = createSlider(0, 2, 1, 0.1)
-  separationSlider.parent(sDiv)
-
-  aDiv = createDiv('Alignment');
-  alignmentSlider = createSlider(0, 2, 1, 0.1);
-  alignmentSlider.parent(aDiv)
-
-  cDiv = createDiv('Cohesion');
-  cohesionSlider = createSlider(0, 2, 1, 0.1);
-  cohesionSlider.parent(cDiv)
-
-  for (let i = 0; i < 200; i++) {
-    flock.push(new Boid());
-  }
+    createCanvas(640, 480);
+    
+    aDiv = createDiv('Alignment');
+    alignmentSlider = createSlider(0, 2, 1, 0.1);
+    alignmentSlider.parent(aDiv)
+    
+    cDiv = createDiv('Cohesion');
+    cohesionSlider = createSlider(0, 2, 1, 0.1);
+    cohesionSlider.parent(cDiv)
+    
+    sDiv = createDiv('Separation');
+    separationSlider = createSlider(0, 2, 1, 0.1)
+    separationSlider.parent(sDiv)
+    
+    for (let i = 0; i < 200; i++) {
+        boids.push(new Boid());
+    }
 }
 
 function draw() {
-  background(51);
-  for (let boid of flock) {
-    boid.edges();
-    boid.flock(flock);
-    boid.update();
-    boid.show();
-  }
+    background(51);
+    for (let boid of boids) {
+        boid.wraparound();
+        boid.flock(boids);
+        boid.update();
+        boid.show();
+    }
 }
