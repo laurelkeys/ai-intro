9f2df95cd4cd93e89984adef42b8a667a4b23bd6
Tiago <tiagoloureirochaves@gmail.com>
2019-05-31 11:39:27 -0300

    Remove FIS tests

--- boids/src/main/kotlin/com/boids/AlignmentControl.kt
+++ boids/src/main/kotlin/com/boids/AlignmentControl.kt
@@ -1,18 +1,15 @@
 package com.boids
 
-import com.fuzzylite.Engine
-import com.fuzzylite.Op
-import com.fuzzylite.FuzzyLite
 import com.fuzzylite.imex.FclImporter
 import com.fuzzylite.variable.InputVariable
 import com.fuzzylite.variable.OutputVariable
 import java.io.File
 import java.io.File.separatorChar
 
-object FuzzyLogic {
+object AlignmentControl {
     private val engine = FclImporter().fromFile(
         File(".${separatorChar}src${separatorChar}fcl${separatorChar}alignment.fcl")
-    )!!
+    )
 
     // antecedent
     val distance: InputVariable
@@ -36,6 +33,9 @@ object FuzzyLogic {
         speedChange = engine.getOutputVariable("sChg")
     }
 
+    fun compute(distance: Int, headingDiff: Int, speedDiff: Int) =
+        compute(distance.toDouble(), headingDiff.toDouble(), speedDiff.toDouble())
+
     fun compute(distance: Double, headingDiff: Double, speedDiff: Double) {
         this.distance.value = distance
         this.headingDiff.value = headingDiff
@@ -43,39 +43,5 @@ object FuzzyLogic {
         compute()
     }
 
-    fun compute() {
-        engine.process()
-        FuzzyLite.logger().info(
-            String.format(
-                "distance.input = %s, headingDiff.input = %s, speedDiff.input = %s -> headingChange.output = %s, speedChange.output = %s",
-                Op.str(distance.value),
-                Op.str(headingDiff.value),
-                Op.str(speedDiff.value),
-                Op.str(headingChange.value),
-                Op.str(speedChange.value)
-            )
-        )
-    }
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-
-
-        distance.value = 0.0
-        headingDiff.value = 0.0
-        speedDiff.value = 0.0
-
-        engine.process()
-
-
-    }
-}
-
-
-fun main(args: Array<String>) {
-    FuzzyLogic.distance.value = 0.0
-    FuzzyLogic.headingDiff.value = 0.0
-    FuzzyLogic.speedDiff.value = 0.0
-
-    FuzzyLogic.process()
+    fun compute() = engine.process()
 }
